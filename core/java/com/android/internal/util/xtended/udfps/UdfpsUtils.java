de11a827301842c76f14732e63cb922a26e1c5e5
onettboots <blackcocopet@gmail.com>
2023-05-02 17:42:42 +0000

    udfps: correctly path
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- core/java/com/android/internal/util/xtended/udfps/UdfpsUtils.java
+++ core/java/com/android/internal/util/xtended/udfps/UdfpsUtils.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2020 The Pixel Experience Project
+* Copyright (C) 2022 SuperiorOS
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.internal.util.xtended.udfps;
+package com.android.internal.util.superior.udfps;
 
 import android.content.Context;
 
